c35d3adf18025ef1491a278eec22c3aba150af70
root <root@ip-172-31-0-62.us-west-2.compute.internal>
2017-04-16 13:51:36 -0400

    Selenium test source added

---
+++ src/test/java/testselenium/com/webdemoTest.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.lang.*;
+
+public class webdemoTest {
+ WebDriver driver;
+
+ @BeforeTest
+ public void StartBrowser_NavURL() {
+  String currentDir = System.getProperty("user.dir");
+  System.out.println(currentDir);
+  String geckoDriverLocation = currentDir + "/src/tools/geckodriver-v0.11.1-linux64/geckodriver";
+  String Xport = System.getProperty("lmportal.xvfb.id", ":1");
+
+  System.setProperty("webdriver.gecko.driver",geckoDriverLocation);
+  FirefoxBinary firefoxBinary = new FirefoxBinary();
+  firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
+  //FirefoxProfile firefoxProfile = new FirefoxProfile();
+  driver = new FirefoxDriver(firefoxBinary, null);
+  //driver = new FirefoxDriver();
+  driver.manage().window().maximize();
+ }
+
+ @AfterTest
+ public void CloseBrowser() {
+  driver.quit();
+ }
+
+ @Test
+ public void testToCompareDoubles() {
+  driver.get("https://www.google.com");
+  try
+   {
+    Thread.sleep(5000);
+   }
+  catch(InterruptedException e)
+   {
+     // this part is executed when an exception (in this example InterruptedException) occurs
+   }
+
+ }
+}
